9445e540d4069664cae1dc400857d7416e1f0408
doranmartin <doranmartin1994@gmail.com>
2023-05-16 15:14:28 -0400

    Server responds with text/html

--- routes.js
+++ routes.js
@@ -1,5 +1,14 @@
 const requestListener = (req, res) => {
-  console.log(req.url, req.method, req.headers)
+  const url = req.url;
+  if (url === "/") {
+    res.setHeader("Content-Type", 'text/html');
+    res.write("<html>");
+    res.write("<head><title>HELLO FRIEEEEND</title></head>");
+    res.write('<body><h1>Hey friendo, did you get the message you were looking for????<p>Please enter what mood you\'re in in the form below!!</p></h1></body>')
+    res.write("</html>");
+    return res.end();
+  }
+
 };
 
 module.exports = requestListener;
